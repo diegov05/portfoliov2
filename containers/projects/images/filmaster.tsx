edc180dc8bdbaebff8034ff549fd88a5cca35997
Diego Vargas <diegovs0504@gmail.com>
2024-10-02 18:19:49 -0400

    Gave priority to project images

--- containers/projects/images/filmaster.tsx
+++ containers/projects/images/filmaster.tsx
@@ -12,14 +12,14 @@ export const FilmasterImage = () => {
         to-[#E499FF33]
         via-[#E499FF]
         lg:max-xl:right-52
-        lg:max-[3000px]:right-80
+        lg:right-80
         blur-3xl
         md:max-lg:w-[20rem]
         md:max-lg:h-[25rem]
         lg:max-xl:w-[25rem]
         lg:max-xl:h-[25rem]
-        lg:max-[3000px]:w-[30rem]
-        lg:max-[3000px]:h-[35rem]
+        lg:w-[30rem]
+        lg:h-[35rem]
         opacity-20
         rounded-full
         -z-10
@@ -27,14 +27,13 @@ export const FilmasterImage = () => {
 
       </div>
       <Image
-        width={640}
-        height={812.33}
+        priority
         alt="filmaster-project"
         src={filmaster}
         className="
         w-[20rem]
         md:max-lg:w-[25rem]
-        lg:max-[3000px]:w-[40rem]
+        lg:w-[40rem]
         h-auto
       " />
     </span>
--- containers/projects/images/spotify.tsx
+++ containers/projects/images/spotify.tsx
@@ -12,14 +12,14 @@ export const SpotifyImage = () => {
         to-[#E499FF33]
         via-[#265743]
         lg:max-xl:right-52
-        lg:max-[3000px]:right-80
+        lg:right-80
         blur-3xl
         md:max-lg:w-[20rem]
         md:max-lg:h-[25rem]
         lg:max-xl:w-[25rem]
         lg:max-xl:h-[25rem]
-        lg:max-[3000px]:w-[30rem]
-        lg:max-[3000px]:h-[35rem]
+        lg:w-[30rem]
+        lg:h-[35rem]
         opacity-20
         rounded-full
         -z-10
@@ -27,14 +27,13 @@ export const SpotifyImage = () => {
 
       </div>
       <Image
-        width={640}
-        height={812.33}
+        priority
         alt="spotify-project"
         src={spotify}
         className="
         w-[20rem]
         md:max-lg:w-[25rem]
-        lg:max-[3000px]:w-[40rem]
+        lg:w-[40rem]
         h-auto
       " />
     </span>
