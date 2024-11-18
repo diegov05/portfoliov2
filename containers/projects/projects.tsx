3b64a657d314ab41a061f61d074d6adf35d8144f
Diego Vargas <diegovs0504@gmail.com>
2024-11-18 17:09:34 -0400

    Fixed live preview button in projects section

--- containers/projects/projects.tsx
+++ containers/projects/projects.tsx
@@ -181,7 +181,7 @@ export const Projects = () => {
                   sm:max-lg:text-[11px] 
                   lg:text-[16px] 
                   ">
-                    Visit Repo
+                    Live Preview
                   </span>
                   <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="svg-size">
                     <path fillRule="evenodd" d="M5.22 14.78a.75.75 0 0 0 1.06 0l7.22-7.22v5.69a.75.75 0 0 0 1.5 0v-7.5a.75.75 0 0 0-.75-.75h-7.5a.75.75 0 0 0 0 1.5h5.69l-7.22 7.22a.75.75 0 0 0 0 1.06Z" clipRule="evenodd" />
