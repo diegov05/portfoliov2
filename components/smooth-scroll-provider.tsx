fa16557c1567c7e9ef555330ee2cdd045138beab
Diego Vargas <diegovs0504@gmail.com>
2025-09-23 19:45:00 -0400

    Update smooth-scroll-provider.tsx

--- components/smooth-scroll-provider.tsx
+++ components/smooth-scroll-provider.tsx
@@ -12,11 +12,9 @@ export default function SmoothScrollProvider({
     const lenis = new Lenis({
       duration: 1.2,
       easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
-      direction: 'vertical',
-      gestureDirection: 'vertical',
-      smooth: true,
-      mouseMultiplier: 1,
-      smoothTouch: false,
+      orientation: 'vertical',
+      gestureOrientation: 'vertical',
+      smoothWheel: true,
       touchMultiplier: 2,
       infinite: false,
     });
